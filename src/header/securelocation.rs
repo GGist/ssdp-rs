decd3bdd831729d23b69b74ce2a315b4edc69390
Ignacio Corderi <icorderi@msn.com>
2016-05-08 14:13:20 -0700

    refactor: Remove unnecesary `unsafe impl` for `Send+Sync`

--- src/header/securelocation.rs
+++ src/header/securelocation.rs
@@ -11,25 +11,21 @@ const SECURELOCATION_HEADER_NAME: &'static str = "SECURELOCATION.UPNP.ORG";
 #[derive(Clone, PartialEq, Eq, Hash, Debug)]
 pub struct SecureLocation(pub String);
 
-unsafe impl Sync for SecureLocation { }
-
-unsafe impl Send for SecureLocation { }
-
 impl Header for SecureLocation {
     fn header_name() -> &'static str {
         SECURELOCATION_HEADER_NAME
     }
-    
+
     fn parse_header(raw: &[Vec<u8>]) -> error::Result<Self> {
         if raw.len() != 1 || raw[0].is_empty() {
-            return Err(Error::Header)
+            return Err(Error::Header);
         }
-        
+
         let owned_bytes = raw[0].clone();
-        
+
         match String::from_utf8(owned_bytes) {
-            Ok(n)  => Ok(SecureLocation(n)),
-            Err(_) => Err(Error::Header)
+            Ok(n) => Ok(SecureLocation(n)),
+            Err(_) => Err(Error::Header),
         }
     }
 }
@@ -37,44 +33,44 @@ impl Header for SecureLocation {
 impl HeaderFormat for SecureLocation {
     fn fmt_header(&self, fmt: &mut Formatter) -> Result {
         try!(fmt.write_str(&self.0));
-        
+
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use hyper::header::{Header};
-    
-    use super::{SecureLocation};
-    
+    use hyper::header::Header;
+
+    use super::SecureLocation;
+
     #[test]
     fn positive_securelocation() {
         let securelocation_header_value = &[b"https://192.168.1.1/"[..].to_vec()];
-        
+
         SecureLocation::parse_header(securelocation_header_value).unwrap();
     }
-    
+
     #[test]
     fn positive_invalid_url() {
         let securelocation_header_value = &[b"just some text"[..].to_vec()];
-        
+
         SecureLocation::parse_header(securelocation_header_value).unwrap();
     }
-    
+
     #[test]
     #[should_panic]
     fn negative_empty() {
         let securelocation_header_value = &[b""[..].to_vec()];
-        
+
         SecureLocation::parse_header(securelocation_header_value).unwrap();
     }
-    
+
     #[test]
     #[should_panic]
     fn negative_invalid_utf8() {
         let securelocation_header_value = &[b"https://192.168.1.1/\x80"[..].to_vec()];
-        
+
         SecureLocation::parse_header(securelocation_header_value).unwrap();
     }
 }
