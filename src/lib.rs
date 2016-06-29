6d5f53da696605c12abe1ce3517212c8187cf4de
Ed Barnard <ebarnard@users.noreply.github.com>
2016-06-29 13:22:49 +0100

    Add lookup_host feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![allow(unused_features)]		
+#![feature(lookup_host)]
+
 //! An asynchronous abstraction for discovering devices and services on a network.
 //!
 //! SSDP stands for Simple Service Discovery Protocol and it is a protocol that uses
