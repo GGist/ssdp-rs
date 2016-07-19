GGist/ssdp-rs

commit 9f5d18e2c4f55b7f57cd940dc4688252fd07a997

Another Regression Fix For Listeners

src/message/search.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 use std::fmt::Debug;
-use std::net::{ToSocketAddrs, SocketAddr, SocketAddrV6, IpAddr, Ipv6Addr};
+use std::net::{ToSocketAddrs, SocketAddr, SocketAddrV6};
 use std::str::FromStr;
 use std::time::Duration;
 use std::io;
@@ -209,23 +209,26 @@ impl SearchListener {
     pub fn listen_on_port(port: u16) -> SSDPResult<SSDPReceiver<SearchRequest>> {
         // Generate a list of reused sockets on the standard multicast address.
         let reuse_sockets = try!(message::map_local(|&addr| match addr {
-            SocketAddr::V4(_) => {
-                let mcast_ip: IpAddr = FromStr::from_str(message::UPNP_MULTICAST_IPV4_ADDR).unwrap();
-                let s = try!(net::bind_reuse((mcast_ip, port)));
+            SocketAddr::V4(v4_addr) => {
+                let sock = try!(net::bind_reuse((*v4_addr.ip(), 0)));
+
+                let mcast_ip = FromStr::from_str(message::UPNP_MULTICAST_IPV4_ADDR).unwrap();
+
                 debug!("Joining ipv4 multicast {} at iface: {}", mcast_ip, addr);
-                try!(net::join_multicast(&s, &addr, &mcast_ip));
-                Ok(Some(s))
+                try!(net::join_multicast(&sock, &addr, &mcast_ip));
+
+                Ok(Some(sock))
             }
-            SocketAddr::V6(n) => {
-                let mcast_ip: Ipv6Addr = FromStr::from_str(message::UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR)
+            SocketAddr::V6(v6_addr) => {
+                let sock = try!(net::bind_reuse((*v6_addr.ip(), port)));
+
+                let mcast_ip = FromStr::from_str(message::UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR)
                                              .unwrap();
-                let mut x = n.clone();
-                x.set_ip(mcast_ip);
-                x.set_port(port);
-                let s = try!(net::bind_reuse(x));
+
                 debug!("Joining ipv6 multicast {} at iface: {}", mcast_ip, addr);
-                try!(net::join_multicast(&s, &addr, &IpAddr::V6(mcast_ip)));
-                Ok(Some(s))
+                try!(net::join_multicast(&sock, &addr, &mcast_ip));
+
+                Ok(Some(sock))
             }
         }));
 

src/message/notify.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 use std::fmt::Debug;
 use std::str::FromStr;
-use std::net::{SocketAddr, IpAddr};
+use std::net::{SocketAddr};
 
 use hyper::header::{Header, HeaderFormat};
 
@@ -129,7 +129,7 @@ impl NotifyListener {
                                              .unwrap();
                 
                 debug!("Joining ipv6 multicast {} at iface: {}", mcast_ip, addr);
-                try!(net::join_multicast(&sock, &addr, &IpAddr::V6(mcast_ip)));
+                try!(net::join_multicast(&sock, &addr, &mcast_ip));
 
                 Ok(Some(sock))
             }

commit afd7a8cddda68aadc0c7fdc5215f35a12edbeba9

fix(search): ipv6 broke in commit 9f5d18e
UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR is a link local address.
On Ipv6, link-local requires a scope with it.

src/message/search.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 use std::fmt::Debug;
-use std::net::{ToSocketAddrs, SocketAddr, SocketAddrV6};
+use std::net::{ToSocketAddrs, SocketAddr, SocketAddrV6, IpAddr, Ipv6Addr};
 use std::str::FromStr;
 use std::time::Duration;
 use std::io;
@@ -220,12 +220,17 @@ impl SearchListener {
                 Ok(Some(sock))
             }
             SocketAddr::V6(v6_addr) => {
-                let sock = try!(net::bind_reuse((*v6_addr.ip(), port)));
+                let mcast_ip: Ipv6Addr = FromStr::from_str(message::UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR)
+                    .unwrap();
 
-                let mcast_ip = FromStr::from_str(message::UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR).unwrap();
+                // clone to preserve interface scope
+                let mut x = v6_addr.clone();
+                x.set_ip(mcast_ip);
+                x.set_port(port);
+                let sock = try!(net::bind_reuse(x));
 
                 debug!("Joining ipv6 multicast {} at iface: {}", mcast_ip, addr);
-                try!(net::join_multicast(&sock, &addr, &mcast_ip));
+                try!(net::join_multicast(&sock, &addr, &IpAddr::V6(mcast_ip)));
 
                 Ok(Some(sock))
             }