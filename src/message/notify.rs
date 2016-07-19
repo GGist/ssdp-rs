GGist/ssdp-rs

commit 3f3c27ec28f59a9f981bc683fdbb49599b4a5e5d

Fix Regression From b8b2e29 Where NotifyListener's Were Bound To The Multicast IP Instead Of The Local IP

src/message/notify.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 use std::fmt::Debug;
 use std::str::FromStr;
-use std::net::{SocketAddr, IpAddr, Ipv6Addr};
+use std::net::{SocketAddr, IpAddr};
 
 use hyper::header::{Header, HeaderFormat};
 
@@ -112,23 +112,26 @@ impl NotifyListener {
     pub fn listen_on_port(port: u16) -> SSDPResult<SSDPReceiver<NotifyMessage>> {
         // Generate a list of reused sockets on the standard multicast address.
         let reuse_sockets = try!(message::map_local(|&addr| match addr {
-            SocketAddr::V4(_) => {
-                let mcast_ip: IpAddr = FromStr::from_str(message::UPNP_MULTICAST_IPV4_ADDR).unwrap();
-                let s = try!(net::bind_reuse((mcast_ip, port)));
+            SocketAddr::V4(v4_addr) => {
+                let sock = try!(net::bind_reuse((*v4_addr.ip(), port)));
+
+                let mcast_ip = FromStr::from_str(message::UPNP_MULTICAST_IPV4_ADDR).unwrap();
+                
                 debug!("Joining ipv4 multicast {} at iface: {}", mcast_ip, addr);
-                try!(net::join_multicast(&s, &addr, &mcast_ip));
-                Ok(Some(s))
+                try!(net::join_multicast(&sock, &addr, &mcast_ip));
+
+                Ok(Some(sock))
             }
-            SocketAddr::V6(n) => {
-                let mcast_ip: Ipv6Addr = FromStr::from_str(message::UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR)
+            SocketAddr::V6(v6_addr) => {
+                let sock = try!(net::bind_reuse((*v6_addr.ip(), port)));
+
+                let mcast_ip = FromStr::from_str(message::UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR)
                                              .unwrap();
-                let mut x = n.clone();
-                x.set_ip(mcast_ip);
-                x.set_port(port);
-                let s = try!(net::bind_reuse(x));
+                
                 debug!("Joining ipv6 multicast {} at iface: {}", mcast_ip, addr);
-                try!(net::join_multicast(&s, &addr, &IpAddr::V6(mcast_ip)));
-                Ok(Some(s))
+                try!(net::join_multicast(&sock, &addr, &IpAddr::V6(mcast_ip)));
+
+                Ok(Some(sock))
             }
         }));
 

src/message/mod.rs
@@ -51,6 +51,7 @@ fn all_local_connectors(multicast_ttl: Option<u32>, filter: IpVersionMode) -> io
     })
 }
 
+/// Invoke the closure for every local address found on the system.
 fn map_local<F, R>(mut f: F) -> io::Result<Vec<R>>
     where F: FnMut(&SocketAddr) -> io::Result<Option<R>>
 {

commit d0e28278a073ec57db38655ca3ca7ff8928a22c1

fix(notify): ipv6 broke in commit 3f3c27e
UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR is a link local address.
On Ipv6, link-local requires a scope with it.

src/message/notify.rs
@@ -1,7 +1,7 @@
 use std::borrow::Cow;
 use std::fmt::Debug;
 use std::str::FromStr;
-use std::net::SocketAddr;
+use std::net::{SocketAddr, IpAddr, Ipv6Addr};
 
 use hyper::header::{Header, HeaderFormat};
 
@@ -123,12 +123,17 @@ impl NotifyListener {
                 Ok(Some(sock))
             }
             SocketAddr::V6(v6_addr) => {
-                let sock = try!(net::bind_reuse((*v6_addr.ip(), port)));
+                let mcast_ip: Ipv6Addr = FromStr::from_str(message::UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR)
+                    .unwrap();
 
-                let mcast_ip = FromStr::from_str(message::UPNP_MULTICAST_IPV6_LINK_LOCAL_ADDR).unwrap();
+                // clone to preserve interface scope
+                let mut x = v6_addr.clone();
+                x.set_ip(mcast_ip);
+                x.set_port(port);
+                let sock = try!(net::bind_reuse(x));
 
                 debug!("Joining ipv6 multicast {} at iface: {}", mcast_ip, addr);
-                try!(net::join_multicast(&sock, &addr, &mcast_ip));
+                try!(net::join_multicast(&sock, &addr, &IpAddr::V6(mcast_ip)));
 
                 Ok(Some(sock))
             }